7f8bcf205ac1cacbeacb38316f87929b9beb9bf8
sathish0706 <107674282+sathish0706@users.noreply.github.com>
2022-07-18 01:17:01 -0700

    Add files via upload

--- day-4-task.js
+++ day-4-task.js
@@ -1,23 +1,23 @@
-// var request=new XMLHttpRequest();
-// request.open("GET","https://restcountries.com/v3.1/all")
-// request.send();
-// request.onload=function(){
-//     var result=JSON.parse(request.response)
-//     console.log(result)
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all")
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response)
+    console.log(result)
 
-// for(i=0; i<result.length; i++){
+for(i=0; i<result.length; i++){
 
-//        console.log(result[i].flags);
+       console.log(result[i].flags);
 
-//        console.log(result[i].name);
+       console.log(result[i].name);
 
-//         console.log(result[i].subregion);
+        console.log(result[i].subregion);
 
-//         console.log(result[i].region);
+        console.log(result[i].region);
 
-//         console.log(result[i].population);   
-//     }
-// }
+        console.log(result[i].population);   
+    }
+}
 
 // //compare JSON
 
